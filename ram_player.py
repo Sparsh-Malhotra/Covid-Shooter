6e88ab5d94b282328766b635b8c5ce9311116dc3
Sparsh Malhotra <sparshmalhotraaa@gmail.com>
2021-05-12 12:50:21 +0530

    Changed tile frame

--- ram_player.py
+++ ram_player.py
@@ -1,18 +1,23 @@
 import pygame
+from build_world import *
 from pygame.locals import *
 
 pygame.init()
 WIDTH=950
 HEIGHT=700
 screen=pygame.display.set_mode((WIDTH,HEIGHT))
+img = pygame.image.load('Assets/ram.png')
+revImg = pygame.transform.flip(img,True,False)
 
 class Player():
-    def __init__(self, x, y):
-        img = pygame.image.load('Assets/ram.png')
+    def __init__(self, x, y,obj1):
+        self.obj1=obj1
         self.image = pygame.transform.scale(img, (120,120))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.width=self.image.get_width()
+        self.height=self.image.get_height()
         self.vel_y = 0
         self.jumped = False
 
@@ -28,9 +33,11 @@ class Player():
         if key[pygame.K_SPACE] == False:
             self.jumped = False
         if key[pygame.K_LEFT]:
-            dx -= 1
+            self.image = pygame.transform.scale(revImg, (120,120))
+            dx -= 3.5
         if key[pygame.K_RIGHT]:
-            dx += 1.5
+            self.image = pygame.transform.scale(img, (120,120))
+            dx += 3.5
 
 
 		#add gravity
@@ -40,19 +47,19 @@ class Player():
         dy += self.vel_y
 
 		#check for collision
-        for tile in world.tile_list:
+        for tile in self.obj1.tile_list:
             if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
                 dx = 0
 			#check for collision in y direction
-			if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+            if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
 				#check if below the ground i.e. jumping
-				if self.vel_y < 0:
-					dy = tile[1].bottom - self.rect.top
-					self.vel_y = 0
+                if self.vel_y < 0:
+                    dy = tile[1].bottom - self.rect.top
+                    self.vel_y = 0
 				#check if above the ground i.e. falling
-				elif self.vel_y >= 0:
-					dy = tile[1].top - self.rect.bottom
-					self.vel_y = 0
+                elif self.vel_y >= 0:
+                    dy = tile[1].top - self.rect.bottom
+                    self.vel_y = 0
 
 		#update player coordinates
         self.rect.x += dx
